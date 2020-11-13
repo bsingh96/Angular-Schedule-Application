b9777498cf6d801f49f91bd51079c7c46d41a34a
Bikramjit Singh <bsingh96@uwo.ca>
2020-11-13 15:39:30 -0500

    added app rounting and a navbar.

---
+++ app-routing/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ManageComponent } from './manage/manage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ManageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
